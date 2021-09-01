d2e343a7ba070b02bc48c174066a075c607d06a8
Shakanksh Sinha <shakankshs@gmail.com>
2021-09-01 09:45:52 +0530

    Landing Page Complete

---
+++ index.js
@@ -0,0 +1,10 @@
+function imgSwitch(shakeImage)
+{
+   document.querySelector('.starbucks').src = shakeImage;
+}
+
+function circleColorChange(color)
+{
+   const circle = document.querySelector('.circle');
+   circle.style.background = color;
+}
