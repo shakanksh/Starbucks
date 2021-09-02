c3835b388f2b3970d825aeee6a4ae0646e51208e
Shakanksh Sinha <shakankshs@gmail.com>
2021-09-02 09:09:49 +0530

    Made Website Responsive

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function circleColorChange(color)
    const circle = document.querySelector('.circle');
    circle.style.background = color;
 }
+
+function toggleMenu()
+{
+   var menuToggle = document.querySelector('.toggle');
+   var navigation = document.querySelector('.navigation');
+   menuToggle.classList.toggle('active');
+   navigation.classList.toggle('active');
+}
